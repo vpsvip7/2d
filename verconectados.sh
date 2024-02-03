c3d226eeb6aec25868d2ffaed0724eff6d3157db
diego <145928215+vpsvip7@users.noreply.github.com>
2024-02-03 14:08:50 -0300

    Actualizar verconectados.sh

--- verconectados.sh
+++ verconectados.sh
@@ -13,14 +13,15 @@ while true; do
   case $opcion in
     1)
       PSIPHON_PORT_80=$(sudo netstat -tn | awk '$4 ~ /:80$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
-      echo "CONEXIONES EN EL PUERTO 80 DE PSIPHON: $PSIPHON_PORT_80"
+      echo "CONEXIONES EN EL PUERTO 80 : $PSIPHON_PORT_80"
       ;;
     2)
       PSIPHON_PORT_443=$(sudo netstat -tn | awk '$4 ~ /:443$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
-      echo "CONEXIONES EN EL PUERTO 443 DE PSIPHON: $PSIPHON_PORT_443"
+      echo "CONEXIONES EN EL PUERTO 443 : $PSIPHON_PORT_443"
       ;;
       3)  
-        wget https://raw.githubusercontent.com/vpsvip7/2d/main/verconectados.sh && chmod +x verconectados.sh &&  ./verconectados.sh  ;;
+        wget https://raw.githubusercontent.com/vpsvip7/2d/main/verconectados.sh && chmod +x verconectados.sh
+      ;;
     4)
       echo "Saliendo del script..."
       break
