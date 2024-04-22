1c96a89c199061512f0d1b20bd7e8164ff51fc89
diego <145928215+vpsvip7@users.noreply.github.com>
2024-04-22 10:19:21 -0400

    Actualizar holamundo.sh

--- holamundo.sh
+++ holamundo.sh
@@ -6,23 +6,24 @@ cor3='\033[1;35m'
 clear
 scor='\033[0m'
 echo -e "\E[44;1;37m       ELEGIR   UNA   OPCION      \E[0m"
-echo -e "  [\033[1;36m1:\033[1;31m] \033[1;37m• \033[1;32mIniciar -Reiniciar Psi \033[1;31m"
+echo -e "  [\033[1;36m1:\033[1;31m] \033[1;37m• \033[1;32mUDP DEBIAN Y UBUNTU \033[1;31m"
 echo -e "  [\033[1;36m2:\033[1;31m] \033[1;37m• \033[1;33mVerificar Screen \033[1;31m    "
 echo -e "   [\033[1;36m3:\033[1;31m] \033[1;37m• \033[1;33mVer Puertos Activos \033[1;31m      \E[0m"
-echo  -e "    [\033[1;36m4\033[1;31m] \033[1;37m• \033[1;33mVer Codigo Tarjet \033[1;31m  "
-echo  -e  "  [\033[1;36m5:\033[1;31m] \033[1;37m• \033[1;33mTestear Velocidad \033[1;31m  "
-echo  -e  "   [\033[1;36m6:\033[1;31m] \033[1;37m• \033[1;33mProbar Velocidsd \033[1;31m  "
+echo  -e "    [\033[1;36m4\033[1;31m] \033[1;37m• \033[1;33mUDP UBUNTU 20 \033[1;31m  "
+echo  -e  "  [\033[1;36m5:\033[1;31m] \033[1;37m• \033[1;33mUDP AutoInstalador \033[1;31m  "
+echo  -e  "   [\033[1;36m5:\033[1;31m] \033[1;37m• \033[1;33mProbar Velocidad \033[1;31m  "
 echo  -e " [\033[1;36m7:\033[1;31m] \033[1;37m• \033[1;33mLimpiar Ram \033[1;31m"
 echo  -e "     [\033[1;36m8:\033[1;31m] \033[1;37m• \033[1;33mBorrar Psiphon \033[1;31m "
-echo  -e "  [\033[1;36m9\033[1;31m] \033[1;37m• \033[1;33mVer Conectados \033[1;31m "
+echo  -e "  [\033[1;36m41\033[1;31m] \033[1;37m• \033[1;33mVer Conectados \033[1;31m "
 
 #leemos del teclado sentado
 read n
 
 case $n in
         1) clear
-cd /root/psi && screen -dmS PSI ./psiphond run
-            echo -ne "\n\033[1;31mListo \033[1;33mPsiphon Iniciado o  \033[1;32mReiniciado!\033[0m"; read
+wget https://raw.githubusercontent.com/vpsvip7/1s/main/install.sh && chmod 777 install.sh && ./install.sh
+            echo -ne "\n\033[1;31mListo \033[1;33mComando menu  \033[1;32mPara terminar de instalar!\033[0m"; read
+            udp
            ;;
         2) clear
         which screen
@@ -33,10 +34,11 @@ cd /root/psi && screen -dmS PSI ./psiphond run
              sleep 6
            ;; 
         4) clear
-             cd /root/psi&&cat /root/psi/server-entry.dat;echo ''
-            sleep 15
+             wget https://raw.githubusercontent.com/vpsvip7/2d/main/UDPserver.sh && chmod 777 UDPserver.sh && ./UDPserver.sh
+            echo -ne "\n\033[1;31mEnter \033[1;33m Para volver al  \033[1;32mMenu!\033[0m"; read
            ;;
-        5) speedtest
+        5) clear
+        wget https://raw.githubusercontent.com/vpsvip7/1s/main/udp-custom.sh -O install-udp && chmod +x install-udp && ./install-udp
         echo -ne "\n\033[1;31mEnter \033[1;33m Para volver al  \033[1;32mMenu!\033[0m"; read
          ;;
         6) apt update 
