6ab844c9e1504ab8724e4249bfa4a6dee66576e1
diego <145928215+vpsvip7@users.noreply.github.com>
2024-04-22 10:42:51 -0400

    Actualizar holamundo.sh

--- holamundo.sh
+++ holamundo.sh
@@ -7,14 +7,22 @@ clear
 scor='\033[0m'
 echo -e "\E[44;1;37m       ELEGIR   UNA   OPCION      \E[0m"
 echo -e "  [\033[1;36m1:\033[1;31m] \033[1;37m• \033[1;32mUDP DEBIAN Y UBUNTU \033[1;31m"
-echo -e "  [\033[1;36m2:\033[1;31m] \033[1;37m• \033[1;33mVerificar Screen \033[1;31m    "
+
+echo -e "  [\033[1;36m2:\033[1;31m] \033[1;37m• \033[1;33mVerificar Screen \033[1;31m  \e[0m  "
+
 echo -e "   [\033[1;36m3:\033[1;31m] \033[1;37m• \033[1;33mVer Puertos Activos \033[1;31m      \E[0m"
-echo  -e "    [\033[1;36m4\033[1;31m] \033[1;37m• \033[1;33mUDP UBUNTU 20 \033[1;31m  "
-echo  -e  "  [\033[1;36m5:\033[1;31m] \033[1;37m• \033[1;33mUDP AutoInstalador \033[1;31m  "
+
+echo  -e "    [\033[1;36m4\033[1;31m] \033[1;37m• \033[1;33mUDP UBUNTU 20 \033[1;31m \e[0m "
+
+echo  -e  "  [\033[1;36m5:\033[1;31m] \033[1;37m• \033[1;33mUDP AutoInstalador \033[1;31m \e[0m  "
+
 echo  -e  "   [\033[1;36m5:\033[1;31m] \033[1;37m• \033[1;33mProbar Velocidad \033[1;31m  "
-echo  -e " [\033[1;36m7:\033[1;31m] \033[1;37m• \033[1;33mLimpiar Ram \033[1;31m"
-echo  -e "     [\033[1;36m8:\033[1;31m] \033[1;37m• \033[1;33mBorrar Psiphon \033[1;31m "
-echo  -e "  [\033[1;36m41\033[1;31m] \033[1;37m• \033[1;33mVer Conectados \033[1;31m "
+
+echo  -e " [\033[1;36m7:\033[1;31m] \033[1;37m• \033[1;33mLimpiar Ram \033[1;31m \e[0m"
+
+echo  -e "     [\033[1;36m8:\033[1;31m] \033[1;37m• \033[1;33mBorrar Psiphon \033[1;31m \e[0m"
+
+echo  -e "  [\033[1;36m41\033[1;31m] \033[1;37m• \033[1;33mVer Conectados \033[1;31m \e[0m"
 
 #leemos del teclado sentado
 read n
