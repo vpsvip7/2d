103bbb024df3afc3f093e6eecbf1bb16fb77a762
diego <145928215+vpsvip7@users.noreply.github.com>
2024-09-19 11:56:27 -0300

    Actualizar verconectados.sh

--- verconectados.sh
+++ verconectados.sh
@@ -5,9 +5,10 @@ while true; do
   echo "Seleccione una opción:"
   echo "1. Ver conectados en el puerto 80"
   echo "2. Ver conectados en el puerto 443"
-  echo "3. Ver conectados en el puerto 22"  
-  echo "4. INSTALAR VER Conectados"
-  echo "5. Salir"
+  echo "3. Ver conectados en el puerto 2052"
+  echo "4. Ver conectados en el puerto 22"  
+  echo "5. INSTALAR VER Conectados"
+  echo "6. Salir"
 
   read -p "Opción: " opcion
 
@@ -21,13 +22,17 @@ while true; do
       echo "CONEXIONES EN EL PUERTO 443 : $PSIPHON_PORT_443"
       ;;
     3)
+      PSIPHON_PORT_2052=$(sudo netstat -tn | awk '$4 ~ /:2052$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
+      echo "CONEXIONES EN EL PUERTO 2052 : $PSIPHON_PORT_2052"
+      ;;  
+   4)
       PSIPHON_PORT_22=$(sudo netstat -tn | awk '$4 ~ /:22$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
       echo "CONEXIONES EN EL PUERTO 22 : $PSIPHON_PORT_22"
       ;;      
-    4)  
-        wget https://raw.githubusercontent.com/vpsvip7/2d/main/verconectados.sh && chmod +x verconectados.sh
+    5)  
+        wget https://raw.githubusercontent.com/vpsvip7/2d/main/verconectados.sh  && chmod +x verconectados.sh
       ;;
-    5)
+    6)
       echo "Saliendo del script..."
       break
       ;;
